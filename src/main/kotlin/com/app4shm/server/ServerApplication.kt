50f1340a13026b5aed4eb048290955624d34eb86
nunopenim <nuno.penim@gmail.com>
2021-03-15 15:55:03 +0000

    ServerApplication: added a DataPusher class with pushData, to print stuff on the HTML

--- src/main/kotlin/com/app4shm/server/ServerApplication.kt
+++ src/main/kotlin/com/app4shm/server/ServerApplication.kt
@@ -2,7 +2,12 @@ package com.app4shm.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+
+var printme : String = ""
+val LINENUMBER = 500
 
 @SpringBootApplication
 class ServerApplication
@@ -10,12 +15,37 @@ fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
 }
 
+fun getHtmlLines() : Int {
+    var count = 0
+    for (char in printme) {
+        if (char == '\n') {
+            count++
+        }
+    }
+    return count
+}
+
 @RestController
 @RequestMapping("/data")
 class DataGetter {
-
     @PostMapping("/reading")
     fun makeData(@RequestBody data: Array<Data>){
-        print(data[0].toString() + data[1].toString())
+        for (i in data) {
+            printme += i.toString()
+            print(i.toString());
+            if (getHtmlLines() > LINENUMBER) {
+                printme = ""
+            }
+        }
+    }
+}
+
+@Controller
+class DataPusher {
+    @GetMapping("/diag")
+    fun pushData(@RequestParam(name = "printme", required = false, defaultValue = "") discarded : String, model : Model){
+        var printing_str : String = printme
+        printing_str = printing_str.replace("\n", "'<br>'")
+        model.addAttribute("printme", printme)
     }
 }
