e4f915017c2116fe8ab8305c35fd1f760b99adfb
nunopenim <nuno.penim@gmail.com>
2021-03-16 10:22:13 +0000

    Data: removed semi-colon, force of habit

--- src/main/kotlin/com/app4shm/server/Data.kt
+++ src/main/kotlin/com/app4shm/server/Data.kt
@@ -2,6 +2,6 @@ package com.app4shm.server
 
 class Data(val id: String, val timeStamp: Long, val x: Float, val y: Float, val z: Float){
     override fun toString(): String {
-        return "ID: ${id} | Timestamp: ${timeStamp} | X: ${x} | Y: ${y} | Z: ${z}\n";
+        return "ID: ${id} | Timestamp: ${timeStamp} | X: ${x} | Y: ${y} | Z: ${z}\n"
     }
 }
