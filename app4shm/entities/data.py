1db2e916644c7cb2d2eb00e5c093026604f79ffc
nunopenim <nuno.penim@gmail.com>
2021-04-17 14:28:46 +0100

    data: apparently I mistakenly ported to python the wrong function

--- app4shm/entities/data.py
+++ app4shm/entities/data.py
@@ -8,5 +8,8 @@ class Data:
         self.group = str(group)
 
     # eww snakecase
-    def to_string(self):
+    def jsoner(self):
         return "{\"id\": \"" + self.identifier + "\", \"timeStamp\": " + str(self.timestamp) + ", \"x\": " + str(self.x) + ", \"y\": " + str(self.y) + ", \"z\": " + str(self.z) + ", \"group\": \"" + self.group + "\"}"
+
+    def to_string(self):
+        return "ID: " + self.identifier + " | group: " + self.group + " | Timestamp: " + str(self.timestamp) + " | X: " + str(self.x) + " | Y: " + str(self.y) + " | Z: " + str(self.z) + "\n"
