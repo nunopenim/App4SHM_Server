cc43564159be607e0c248e6aae083c46ca43f6fa
nunopenim <nuno.penim@gmail.com>
2021-05-30 13:58:10 +0100

    mathematics: added a basic interpolation function
    - Ported from Professor Ionut's Matlab script, using scipy and my basic knowledge on how it works

---
+++ app4shm/sys_helpers/mathematics.py
@@ -0,0 +1,44 @@
+# App4SHM Server - Mathematics
+#
+# This class will allow us to do the needed mathematic operations, such as
+# Interpolation, in order to synchronize readings to the required time.
+#
+# Nuno Penim, Paulo Oliveira, 2021
+#
+# No tabs allowed for the safety of the entire project
+# Use 4 spaces as indentation (I KNOW, BUT THAT'S HOW PYTHON ROLLS, I AM SORRY)
+
+from scipy.interpolate import interpn as interpn
+from app4shm.entities.data import Data
+
+# Constants and Parameters
+INTERPOLATION_TYPE = 'linear'
+TIME_INCREMENT = 10  # 10 ms
+
+
+def interpolate_data_stream(data_stream : list[Data]):
+    data_device = data_stream[0].identifier
+    data_group = data_stream[0].group
+    data_times = []
+    data_x = []
+    data_y = []
+    data_z = []
+    for i in data_stream:
+        data_times.append(i.timestamp)
+        data_x.append(i.x)
+        data_y.append(i.y)
+        data_z.append(i.z)
+    t_start = data_times[0]
+    t_end = data_times[len(data_times)-1]
+    t_interval = []
+    start_me = t_start
+    while start_me < t_end:
+        t_interval.append(start_me)
+        start_me += TIME_INCREMENT
+    inter_x = interpn(data_times, data_x, t_interval, INTERPOLATION_TYPE)
+    inter_y = interpn(data_times, data_y, t_interval, INTERPOLATION_TYPE)
+    inter_z = interpn(data_times, data_x, t_interval, INTERPOLATION_TYPE)
+    ret_stream = []
+    for i in range(0, len(t_interval)):
+        ret_stream.append(Data(identifier=data_device, timestamp=t_interval[i], x=inter_x[i], y=inter_y[i], z=inter_z[i], group=data_group))
+    return ret_stream
