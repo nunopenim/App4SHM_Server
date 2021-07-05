65b8bc24503d746e37a1793f0acfbfff525b0065
nunopenim <nuno.penim@gmail.com>
2021-07-05 14:15:46 +0100

    general: initial db sketch

--- app4shm/entities/medicao_db.py
+++ app4shm/entities/medicao_db.py
@@ -1,9 +1,8 @@
 class Medicao:
-    def __init__(self, identifier, timestamp, x, y, z, group, resultado_id):
+    def __init__(self, identifier, timestamp, x, y, z, resultado_id):
         self.identifier = str(identifier)
         self.timestamp = int(timestamp)
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
         self.resultado_id = resultado_id  # Foreign key do resultado
-        self.group = str(group)
--- app4shm/entities/resultado_db.py
+++ app4shm/entities/resultado_db.py
@@ -1,4 +1,5 @@
 class Resultado:
-    def __init__(self, ident, id_group):
-        self.ident = ident  # timestamp do inicio
+    def __init__(self, ident, timestamp_start, id_group):
+        self.ident = ident  # primary key para a estrutura
+        self.timestamp_start = timestamp_start
         self.id_group = id_group  # id do group como foreign key
